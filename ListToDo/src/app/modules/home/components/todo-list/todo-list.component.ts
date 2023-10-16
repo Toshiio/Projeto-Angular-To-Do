840a89cfd03c80a6e0c79aaacddcde1a13b10f44
mateus.shibata <mateus.shibata@tecnogroup.com.br>
2023-10-16 13:25:14 -0300

    Adicionado Local Storage e finalizado projeto base

--- ListToDo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ ListToDo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 //interface
 import { TaskList } from '../../model/task-list';
@@ -8,18 +8,21 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova task", checked: true},
-    { task: "Minha nova task 2", checked: false}
-  ];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
   constructor(){ }
 
-  ngOnInit(): void {
-    
+  ngDoCheck(){
+    if(this.taskList){
+      this.taskList.sort( ( first, last ) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
   }
 
+  public setEmmitTaskList(event: string){
+    this.taskList.push({task: event, checked: false});
+  }
   public deleteItemTaskList(event: number){
     this.taskList.splice(event, 1);
   }
@@ -29,6 +32,24 @@ export class TodoListComponent implements OnInit {
 
     if(confirm == true){
     this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number){
+
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja Deletar?");
+      
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
+  public setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort( ( first, last ) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 
